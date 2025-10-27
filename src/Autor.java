31aebc408a5b5e35200581b6ea7bb9e00168b08e
Leonardo Rocha Vieira <rochavieira08@gmail.com>
2025-10-27 13:24:28 -0300

    Update Autor.java

--- src/Autor.java
+++ src/Autor.java
@@ -1,4 +1,29 @@
 package PACKAGE_NAME;
 
+  import java.time.LocalDate;
+
 public class Autor {
+    private static int contador = 1;
+    private final int id;
+    private String nomeDoAutor;
+    private LocalDate dataNascimento;
+
+    public Autor(String nomeDoAutor, LocalDate dataNascimento){
+        this.nomeDoAutor = nomeDoAutor;
+        this.dataNascimento = dataNascimento;
+        this.id = contador++;
+    }
+    public String getNomeDoAutor() {
+    return nomeDoAutor;
+    }
+    public int getId(){
+        return id;
+    }
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+    public void setDataNascimento(LocalDate dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
 }
