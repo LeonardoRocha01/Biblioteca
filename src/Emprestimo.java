3392a02dd65d565ef3f5ebf1b24a88d953fa31fd
Leonardo Rocha Vieira <rochavieira08@gmail.com>
2025-10-27 13:25:46 -0300

    Add Emprestimo class for loan management

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,4 +1,46 @@
 package PACKAGE_NAME;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Emprestimo {
+private static int contador = 1;
+private final int id;
+private Livro livro;
+private String nomeCliente;
+private LocalDate dataDeEmprestimo;
+private LocalDate dataDevolucao;
+private LocalDate dataPrevista;
+protected static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+public Emprestimo(Livro livro, String nomeCliente, LocalDate dataPrevista) {
+    this.id = contador++;
+    this.livro = livro;
+    this.nomeCliente = nomeCliente;
+    this.dataDeEmprestimo = LocalDate.now();
+    this.dataDevolucao = null;
+    this.dataPrevista = dataPrevista;
+}
+public int getId() {
+    return id;
+}
+
+public String getDataPrevista() {
+    return dataPrevista.format(formatter);
+}
+public Livro getLivro(){
+    return livro;
+}
+public String getNomeCliente(){
+    return nomeCliente;
+}
+public LocalDate getDataDeEmprestimo(){
+    return dataDeEmprestimo;
+}
+public LocalDate getDataDevolucao(){
+    return dataDevolucao;
+}
+public void setDataDevolucao(LocalDate dataDevolucao){
+    this.dataDevolucao = dataDevolucao;
+}
 }
